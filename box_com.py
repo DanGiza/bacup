0fcadda495a33166fabe0f223fca208fb78ce61b
DanGiza <dangiza@gmail.com>
2013-09-26 00:10:33 +0300

    Initial commit

---
+++ box_com.py
@@ -0,0 +1,68 @@
+import requests
+import webbrowser
+import simplejson as json
+# Legend
+# Start of var: str-string, nr-number, pw-password!,
+# End of var: l-link, t-text, o-object
+#
+
+#o_brw_Browser = mechanize.Browser()
+class BOX:
+    """
+    www.box.com api
+    works:
+        ->  login
+        ->  get token w/ error handling.
+    to-do:
+        ->  upload/download/explore.
+    """
+
+    str_BuildGet        = 'https://www.box.com/api/oauth2/authorize?'
+    str_BuildToken      = 'https://www.box.com/api/oauth2/token'
+    str_redirect_uri    = 'https://www.box.com/developers/services'
+    str_response_type   = 'response_type=code'
+    str_state           = 'authenticated'
+    str_client_id       = 'j11v31md50xohags558ue1t2bteto63w'
+    str_client_secret   = 'awHdRpTm7eMslhv4ZelwxT81GIhHsXV2'
+    str_grant_type      = 'authorization_code'
+    str_code            = ''
+
+    def __init__(self):
+        self.str_BuildGet = self.str_BuildGet + self.str_response_type + '&' + 'client_id=' + self.str_client_id + '&' + 'state=' + self.str_state
+        self.str_code = raw_input('Enter code parameter (code=something) from URL: ')
+
+    def GetCODE(self):
+        webbrowser.open_new_tab(self.str_BuildGet)
+
+    def GetTOKEN(self):
+        """
+        returns 2 parameters:
+        1 -> 0/1 : nok/ok
+        2 -> server response
+        """
+        payload_GetRequest = {'grant_type'      : self.str_grant_type,
+                              'code'            : self.str_code,
+                              'client_id'       : self.str_client_id,
+                              'client_secret'   : self.str_client_secret}
+        o_MyRequest = requests.post(self.str_BuildToken, data = payload_GetRequest)
+
+        try:
+            responseDict = json.loads(o_MyRequest.text)
+        except Exception, err:
+            print 'ERROR at handling POST response data: \n' % err
+            return 0, o_MyRequest.text
+
+        try:
+            if responseDict['access_token']:
+                return 1, responseDict
+        except Exception, err:
+            print 'ERROR:GetTOKEN() -> POST response:\n' % err
+            return 0, o_MyRequest.text
+
+    def BOX_LogIn(self):
+        self.GetCODE()
+        errorCode, response = self.GetTOKEN()
+        if errorCode == 1:
+            return
+        else:
+            print 'ERROR:GetTOKEN() -> POST response:\n' % response
---
+++ main.py
@@ -0,0 +1,7 @@
+from box_com import BOX
+__author__ = 'Dan'
+
+
+if __name__ == "__main__":
+    temp = BOX()
+    temp.BOX_LogIn()
